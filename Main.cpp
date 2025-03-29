bd0bdc42f4155266a6406f1a1ab983863f20b83c
Moseph90 <mlochness90@gmail.com>
2025-03-29 10:38:46 -0500

    Changed it to color and removed the window

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 #define WIDTH 1280
 #define HEIGHT 720
-#define FRAMEW 100
+#define FRAMEW 150
 #define FRAMEH 45
 
 #include <iostream>
@@ -13,36 +13,26 @@ using namespace cv;
 void ASCIIconvert(Mat& frame) {
     for (int i = 0; i < frame.rows; i++) {
         for (int j = 0; j < frame.cols; j++) {
-            int greyscale = frame.at<uchar>(i, j);
+            Vec3b color = frame.at<Vec3b>(i, j); 
+            
+            int r = color[0];
+            int g = color[1];
+            int b = color[2];
 
-            char ascii = '.';
-
-            if (greyscale < 16) ascii = '#';
-            else if (greyscale < 32) ascii = '$';
-            else if (greyscale < 48) ascii = '@';
-            else if (greyscale < 64) ascii = '%';
-            else if (greyscale < 80) ascii = '&';
-            else if (greyscale < 96) ascii = '*';
-            else if (greyscale < 128) ascii = '+';
-            else if (greyscale < 150) ascii = 'a';
-            else if (greyscale < 180) ascii = '0';
-            else if (greyscale < 216) ascii = '-';
-            else if (greyscale < 255) ascii = '.';
-
-            std::cout << ascii;
+            std::cout << "\033[38;2;" << r << ";" << g << ";" << b << "m#"; 
         }
-        std::cout << std::endl;
+        std::cout << std::endl; 
     }
     std::cout << "\x1b[H";
 }
 
 int main(int argc, char** argv) {
     
-    std::string video_path = "Elephant.mp4";
+    std::string video_path = "Swans.mp4";
     VideoCapture capture(video_path);
 
     if (!capture.isOpened())
-        std::cout << "Error when reading Parkour.mp4" << std::endl;
+        std::cout << "Error when reading video" << std::endl;
     else {
         int fps = capture.get(5);
         std::cout << "Frames per second: " << fps << std::endl;
@@ -50,28 +40,20 @@ int main(int argc, char** argv) {
         std::cout << "Frame count: " << frame_count << std::endl;
     }
 
-    namedWindow("Frame", WINDOW_NORMAL); // Normal window, resizable
-
-    //Resize the window to a specific size (e.g., 640x480)
-    resizeWindow("Frame", WIDTH, HEIGHT);
-
     while(capture.isOpened()) {
         Mat frame;
-        Mat greyFrame;
-        Mat resizedFrame;
+        Mat resizedColorFrame;
         bool areFrames = capture.read(frame);
 
         if (areFrames) {
-            cvtColor(frame, greyFrame, cv::COLOR_BGR2GRAY);
-            resize(greyFrame, resizedFrame, Size(FRAMEW, FRAMEH), INTER_LINEAR);
-            imshow("Frame", frame);
-            ASCIIconvert(resizedFrame);
+            resize(frame, resizedColorFrame, Size(FRAMEW, FRAMEH), INTER_LINEAR);
+            ASCIIconvert(resizedColorFrame);
         }
         else  
             capture.set(CAP_PROP_POS_FRAMES, 0);
-
+        
+        //These only work if there's a window present
         int key = waitKey(20);
-
         if (key == 'q') break;
 
         std::this_thread::sleep_for(std::chrono::milliseconds(33));
--- README.md
+++ README.md
@@ -2,16 +2,12 @@ Hi!
 
 Welcome to the ASCII video converter repository. This program converts videos to ASCII art. There are a few things to note before you run this program.
 
-***Closing the window does not end the program, press 'q' to end it!***
-
 1. You will need to link open cv to your project as it's not included here.
 2. The video requires a full screen terminal to be seen properly due to the amount of characters being printed to the screen.
-3. Due to the nature of ASCII characters, results will vary depending on the video.
-4. I recommend high contrast videos for best results.
-5. I added a make file for those of you who wish to run this in linux (you will still need to add open cv though).
-6. There is no video provided (to save space in github) so you will need to save one to the same folder as Main.cpp
-7. Rename the video_path variable located at the start of the main function to match the ovideo you added.
-8. This program has not been tested in Windows, only Linux, and it works great.
+3. I added a make file for those of you who wish to run this in linux (opencv is in the make file but you will still need to add open cv to your system).
+4. There is no video provided (to save space in github) so you will need to save one to the same folder as Main.cpp
+5. Rename the video_path variable located at the start of the main function to match the video you added.
+6. This program has not been tested in Windows, only Linux, and it works great.
 
 If the program starts flickering, it's because your terminal has no room for the characters and it keeps scrolling. If you need to, mess with the FRAMEW and FRAMEH macros at the top of the program to change the ASCII art width and height, but do your best to keep it near 20:9 or 21:9. 16:9 videos work best, but you should make it wider since to account for line spacing being more than letter spacing.
 
@@ -28,9 +24,3 @@ ASCIIconvert(resizedFrame);
 Then you'll be able to see the frame rate and frame count displayed in the terminal.
 
 Thank you for checking out my repository and enjoy.
-
-
-
-
-https://github.com/user-attachments/assets/c665f831-89d6-4ccb-b756-9b3bc0a65bbd
-
